5e80cfd09987613d0aa07fd3bcd0f59346f6e1dc
Maryam-Ketatni-004 <maryamketatni00.com>
2023-12-16 15:19:25 -0800

    ii

--- opcodes.c
+++ opcodes.c
@@ -108,4 +108,60 @@ void _pint (stack_t **top, unsigned int line_number)
  * @line_number: line number of opcode
  * Return: none
  */
+void _pop(stack_t **top, unsigned int line_number)
+{
+	if (!*top)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		free_stack(datax.top);
+		exit(EXIT_FAILURE);
+	}
+	if ((*top)->next == NULL)
+	{
+		free(*top);
+		*top = NULL;
+	}
+	else
+	{
+		*top = (*top)->next;
+		free((*top)->prev);
+		(*top)->prev = NULL;
+	}
+}
+/**
+ * _swap - swaps the top two elements of the stack.
+ * @top: head of double list
+ * @list_number: line number of opcode
+ * Return: none
+ */
+void _swap(stack_t **top, usigned int line_number)
+{
+	stack_t *tmp1;
+
+	if (!*top || (*top)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		free_stack(datax.top);
+		exit(EXIT_FAILURE);
+	}
+	tmp1 = *top;
+	tmp1->prev = tmp1->next;
+	if ((*top)->next->next == NULL)
+		tmp1->next = NULL;
+	else
+	{
+		tmp1->next = tmp1->next->next;
+		tmp1->next->prev = *top;
+	}
+	*top = tmp1->prev;
+	(*top)->prev = NULL;
+	(*top)->next = tmp1;
+}
+
+/**
+ * _add - adds the top two elements of the stack.
+ * @top: head of double list
+ * @line_number: line number of opcode
+ * Return: none
+ */
 
