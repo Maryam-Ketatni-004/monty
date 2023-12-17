970eb3c686bd5c0684615596022e9e5e2c5caaca
Maryam-Ketatni-004 <maryamketatni00.com>
2023-12-17 09:10:53 -0800

    ty

--- opcodes.c
+++ opcodes.c
@@ -164,4 +164,153 @@ void _swap(stack_t **top, usigned int line_number)
  * @line_number: line number of opcode
  * Return: none
  */
+void _add(stack_t **top, unsigned int line_number)
+{
+	int value = 0;
 
+	if (!*top || (*top)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		free_stack(datax.top);
+		exit(EXIT_FAILURE);
+	}
+	value = (*top)->n;
+	_pop(&datax.top, datax.line_num);
+	(*top)->n += value;
+}
+
+/**
+ * _nop - doesn't do anything.
+ * @top: head of double list
+ * @line_number: line number of opcode
+ * Return: none
+ */
+void _nop(stack_t **top, unsigned int line_number)
+{
+	(void)top;
+	(void)line_number;
+}
+
+/**
+ * _sub - subtracts the top element of the stack
+ * from the second top element of the stack.
+ * @top: head of double list
+ * @line_number: line number of opcode
+ * Return: non
+ */
+void _sub(stack_t **top, unsigned int line_number)
+{
+	int value = 0;
+
+	if (!*top || (*top)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		free_stack(datax.top);
+		exit(EXIT_FAILURE);
+	}
+	value = (*top)->n;
+	_pop(&datax.top, datax.line_num);
+	(*top)->n -= value;
+}
+
+/**
+ * _div - divides the second top element of
+ * the stack by the top element of the stack.
+ * @top: head of double list
+ * @line_number: line number of opcode
+ * Return: none
+ */
+void _div(stack_t **top, unsigned int line_number)
+{
+        int value = 0;
+	
+	if (!*top || (*top)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		free_stack(datax.top);
+		exit(EXIT_FAILURE);
+	}
+	value = (*top)->n;
+	_pop(&datax.top, datax.line_num);
+	(*top)->n /= value;
+}
+
+/**
+ * _mul - multiplies the second top element
+ * of the stack with the top element of the stack.
+ * @top: head of double list
+ * @line_number: line number of opcode
+ * Return: none
+ */
+void _mul(stack_t **top, unsigned int line_number)
+{
+	int value = 0;
+
+	if (!*top || (*top)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		free_stack(datax.top);
+		exit(EXIT_FAILURE);
+        }
+        value = (*top)->n;
+        _pop(&datax.top, datax.line_num);
+        (*top)->n *= value;
+}
+
+/**
+ * _mod - computes the rest of the division of the second
+ * top element of the stack by the top elemnebt of the stack.
+ * @top: head of double list
+ * @line_number: line number of opcode
+ * Return: none
+ */
+void _mod(stack_t **top, unsigned int line_number)
+{
+	int value = 0;
+
+	if (!*top || (*top)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		free_stack(datax.top);
+		exit(EXIT_FAILURE);
+	}
+	if ((*top)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free_stack(datax.top);
+		exit(EXIT_FAILURE);
+	}
+	value = (*top)->n;
+	_pop(&datax.top, datax.line_num);
+	(*top)->n %= value;
+}
+
+/**
+ * _pchar - prints the char at the top of the stack (ascii).
+ * @top: head of double list
+ * @line_number: line number of opcode
+ * Return: none
+ */
+void _pchar(stack_t **top, unsigned int line_number)
+{
+	if (!*top)
+	{
+		printf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		free_stack(datax.top);
+		exit(EXIT_FAILURE);
+	}
+	if ((*top)->n > 127 || (*top)->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		free_stack(datax.top);
+		exit(EXIT_FAILURE);
+	}
+	if ((*top)->n > 127 || (*top)->n < 0)
+	{
+		printf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+                free_stack(datax.top);
+                exit(EXIT_FAILURE);
+
+	}
+	printf("%c\n", (*top)->n);
+}
